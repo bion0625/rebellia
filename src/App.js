288a69d9c95670c70a50dd188bc19aff8ea04143
이의정 <stxtory@gmail.com>
2023-09-16 17:00:34 +0900

    rebellia react init

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import img1 from './img/1.jpeg';
+import img2 from './img/Resized_20221122_130409.jpeg';
+import { useState } from 'react';
+import style from './App.module.css'
+
+const Work = ({work, goToggle, index, page}) => {
+  return (
+    <div onClick={goToggle} data-index={index}>
+        <h3 data-index={index}>{work.title}</h3>
+        <div className={style.container} data-index={index}>
+          <img src={work.imgUrl} style={{width:page === "home" ? "10%":"50%"}} data-index={index}/>
+          {page !== "home" ? <div data-index={index}>{work.description}</div> : null}
+        </div>
+      </div>
+  );
+}
+
+const NavBar = ({goHome}) => {
+  return (<nav><span onClick={goHome}>HOME</span></nav>);
+}
+
+function App() {
+  const [page, setPage] = useState("home");
+
+  const goHome = () => {
+    setPage("home");
+  }
+
+  const goToggle = (event) => {
+    console.log(event.target.dataset.index);
+    setPage(event.target.dataset.index);
+  }
+  const works = [
+    {
+      imgUrl:img1,
+      title:"첫 번째 테스트",
+      description:"내용 : "
+    },
+    {
+      imgUrl:img2,
+      title:"두 번째 테스트",
+      description:"내용 : "
+    },
+    {
+      imgUrl:img1,
+      title:"첫 번째 테스트",
+      description:"내용 : "
+    },
+    {
+      imgUrl:img2,
+      title:"두 번째 테스트",
+      description:"내용 : "
+    },
+    {
+      imgUrl:img1,
+      title:"첫 번째 테스트",
+      description:"내용 : "
+    },
+    {
+      imgUrl:img2,
+      title:"두 번째 테스트",
+      description:"내용 : "
+    }
+  ];
+  return (
+    <div>
+      <NavBar goHome={goHome} />
+      <h1>Rebellia</h1>
+      {page === "home" ? works.map((work, index) => (
+          <Work key={index} work={work} goToggle={goToggle} index={index} page={page}/>
+      )) : <Work work={works[Number(page)]} index="home" page={page} goToggle={goToggle}/>}
+    </div>
+  );
+}
+
+export default App;
